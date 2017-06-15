c061aac39eb2080aa0f4cbc7a98f2c6290216810
Daniel Lundby <daniellundby@Daniels-MacBook-Pro.local>
2017-06-14 20:44:37 -0600

    almost done

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -37,7 +37,7 @@ $( "#new-button-form" ).submit(function(event) {
 })
 
 function grabGifs(searchTerm){
-	var queryURL = "http://api.giphy.com/v1/gifs/search?q=" +
+	var queryURL = "https://api.giphy.com/v1/gifs/search?q=" +
     	searchTerm + "&api_key=dc6zaTOxFJmzC&limit=10";
 
 	$.ajax({

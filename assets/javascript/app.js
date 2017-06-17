8dbff40f9f04628b8d026714e7763a83f9296703
Daniel Lundby <daniellundby@Daniels-MacBook-Pro.local>
2017-06-17 12:50:31 -0600

    almost finished

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -79,8 +79,25 @@ function grabGifs(searchTerm){
     });
 }
 
+    $("#Gif").on("click", function() {
+      // The attr jQuery method allows us to get or set the value of any attribute on our HTML element
+      var state = $(this).attr("data-state");
+      // If the clicked image's state is still, update its src attribute to what its data-animate value is.
+      // Then, set the image's data-state to animate
+      // Else set src to the data-still value
+      if (state === "still") {
+        $(this).attr("src", $(this).attr("data-animate"));
+        $(this).attr("data-state", "animate");
+      } else {
+        $(this).attr("src", $(this).attr("data-still"));
+        $(this).attr("data-state", "still");
+      }
+    });
+
 $("#buttons-container > button").click(function(event){
 	grabGifs(event.currentTarget.innerText)
 })
+
+
 	
 })
